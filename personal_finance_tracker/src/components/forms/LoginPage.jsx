f9b2c83438b72b57d36c27d996d3f0d2ec7a0be1
ImaniW <imani@ImaniWs-MacBook-Pro.local>
2021-07-12 10:28:54 -0400

    created real time database in firebase to store tracker info

--- personal_finance_tracker/src/components/forms/LoginPage.jsx
+++ personal_finance_tracker/src/components/forms/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase1 from '../firebase/Firebase';
 
 
 class LoginPage extends Component {
@@ -6,27 +7,44 @@ class LoginPage extends Component {
 
     state = {
         email: '',
-        password: ''
+        password: '',
+        errorsInFireBase: ''
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    signIn = e => {
+        e.preventDefault()
+        firebase1.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .catch((error) => {
+                this.setState({ errorsInFireBase: error.message })
+            })
     }
 
 
     render() {
+
         return (
             <div>
                 <form className="loginForm">
 
                     <div className="form-group">
                         <label htmlFor="email"></label>
-                        <input type="text" className="email" placeholder="Email" name="email" />
+                        <input type="text" className="email" placeholder="Email" name="email"
+                            onChange={this.handleChange} value={this.state.email}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="password"></label>
-                        <input type="text" className="password" placeholder="Password" type="password" name="password" />
+                        <input type="text" className="password" placeholder="Password" type="password" name="password" onChange={this.handleChange} value={this.state.password}
+                        />
                     </div>
 
                     <div className="form-group">
-                        <input type="submit" className="submitButton" value="Submit" />
+                        <input onClick={this.signIn} type="submit" className="submitButton" value="Submit" />
                     </div>
                 </form>
             </div>
