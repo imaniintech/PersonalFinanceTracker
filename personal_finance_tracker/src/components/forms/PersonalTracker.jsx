e26c305c2669e52b76668eb1891fc616d8a067a7
ImaniW <imani@ImaniWs-MacBook-Pro.local>
2021-07-12 11:00:51 -0400

    created a transaction form for new transactions

--- personal_finance_tracker/src/components/forms/PersonalTracker.jsx
+++ personal_finance_tracker/src/components/forms/PersonalTracker.jsx
@@ -13,10 +13,35 @@ class PersonalTracker extends Component {
 
     render() {
         return (
-            <div>
+            <div className="personalTracker">
 
-                <button onClick={this.signOut}>Sign Out</button>
+                <div className="dashboard">
+                    <h4>Hi!</h4>
+                    <button className="signOut" onClick={this.signOut}>Sign Out</button>
+                </div>
 
+                <div className="total"><h3>$1000</h3></div>
+
+                <div className="transactionForm">
+                    <div className="newTransactionForm">
+
+                        <form>
+                            <input placeholder="Transaction Name" type="text" name="nameOfTransaction" />
+
+                            <div className="">
+                                <select name="type">
+                                    <option value="0">Transaction Type?</option>
+                                    <option value="0">Deposit</option>
+                                    <option value="0">Expense</option>
+                                </select>
+                                <input placeholder="Amount" type="text" name="amountOfTransaction" />
+                            </div>
+
+                            <button className="addNewTransaction"> Add Transaction</button>
+                        </form>
+
+                    </div>
+                </div>
             </div>
         )
     }
