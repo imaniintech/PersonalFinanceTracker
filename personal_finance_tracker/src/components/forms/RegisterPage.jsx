7fae77ca7723e3f44ea6a6b13d294c09fa4cbc31
ImaniW <imani@ImaniWs-MacBook-Pro.local>
2021-07-12 06:29:46 -0400

    added method to register page for submit button to send info to firebase

--- personal_finance_tracker/src/components/forms/RegisterPage.jsx
+++ personal_finance_tracker/src/components/forms/RegisterPage.jsx
@@ -11,6 +11,12 @@ class RegisterPage extends Component {
         password: ''
     }
 
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
             <div>
@@ -18,22 +24,30 @@ class RegisterPage extends Component {
 
                     <div className="form-group">
                         <label htmlFor="firstName"></label>
-                        <input type="text" className="firstName" placeholder="First Name" name="name" />
+                        <input type="text" className="firstName" placeholder="First Name" name="firstName"
+                            onChange={this.handleChange} value={this.state.firstName}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="lastName"></label>
-                        <input type="text" className="lastName" placeholder="Last Name" name="name" />
+                        <input type="text" className="lastName" placeholder="Last Name" name="lastName"
+                            onChange={this.handleChange} value={this.state.lastName}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="email"></label>
-                        <input type="text" className="email" placeholder="Email" name="email" />
+                        <input type="text" className="email" placeholder="Email" name="email"
+                            onChange={this.handleChange} value={this.state.email}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="password"></label>
-                        <input type="text" className="password" placeholder="Password" name="password" />
+                        <input type="text" className="password" placeholder="Password" name="password"
+                            onChange={this.handleChange} value={this.state.password}
+                        />
                     </div>
 
                     <div className="form-group">
