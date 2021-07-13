b2105cc61fc76fca9c89120a29c0aaf379d558c4
ImaniW <imani@ImaniWs-MacBook-Pro.local>
2021-07-13 07:50:11 -0400

    added method for new transactions and assigned transaction variables to a transaction state to push to my transactionList array.

--- personal_finance_tracker/src/components/forms/PersonalTracker.jsx
+++ personal_finance_tracker/src/components/forms/PersonalTracker.jsx
@@ -5,27 +5,63 @@ import firebase1 from '../firebase/Firebase';
 
 class PersonalTracker extends Component {
 
+    state = {
+        nameOfTransaction: '',
+        amountOfTransaction: '',
+        transactionType: '',
+        currentUserID: firebase1.auth().currentUser.uid,
+        availableMoney: 0,
+        transactionList: []
+
+    }
 
     signOut = () => {
         firebase1.auth().signOut()
     }
 
-    state = {
-        availableMoney: 0,
-        transactionList: [],
-        nameOfTransaction: '',
-        amountOfTransaction: '',
-        type: '',
-        currentUserID: firebase1.auth().currentUser.uid
-    }
+
+
+
+
+    handleChange = input => e => {
+        this.setState({
+            [input]: e.target.value !== '0' ? e.target.value : ''
+        });
+    };
+
+
+    addANewTransaction = () => {
+        const {
+            nameOfTransaction,
+            amountOfTransaction,
+            transactionType,
+            currentUserID
+        } = this.state;
+
+        if (nameOfTransaction && amountOfTransaction && transactionType) {
+            const transactionState = this.state.transactionList
+            transactionState.push({
+                newID: transactionState.length + 1,
+                name: nameOfTransaction,
+                amount: amountOfTransaction,
+                type: transactionType,
+                currentID: currentUserID
+            })
+            console.log(transactionState)
+        }
+    };
+
+
 
 
     render() {
+
+
         return (
             <div className="personalTracker">
 
                 <div className="dashboard">
-                    <h4>Hi!</h4>
+                    <h4>Welcome!</h4>
                     <button className="signOut" onClick={this.signOut}>Sign Out</button>
                 </div>
 
@@ -35,18 +71,21 @@ class PersonalTracker extends Component {
                     <div className="newTransactionInfo">
 
                         <form>
-                            <input placeholder="Transaction Name" type="text" name="nameOfTransaction" />
+                            <input placeholder="Transaction Name" type="text" name="nameOfTransaction"
+                                onChange={this.handleChange('nameOfTransaction')} value={this.state.nameOfTransaction} />
 
                             <div className="transCluster">
-                                <select name="type">
-                                    <option value="0">Transaction Type?</option>
-                                    <option value="0">Deposit</option>
-                                    <option value="0">Expense</option>
+                                <select name="type"
+                                    onChange={this.handleChange('transactionType')} value={this.state.transactionType}>
+                                    <option value="0">Transaction Type</option>
+                                    <option value="deposit">Deposit</option>
+                                    <option value="expense">Expense</option>
                                 </select>
-                                <input placeholder="Amount" type="text" name="amountOfTransaction" />
+                                <input placeholder="Amount" type="text" name="amountOfTransaction"
+                                    onChange={this.handleChange('amountOfTransaction')} value={this.state.amountOfTransaction} />
                             </div>
 
-                            <button className="addNewTransaction"> Add Transaction</button>
+                            <button className="addANewTransaction" onClick={() => this.addANewTransaction()}> Add Transaction</button>
                         </form>
 
                     </div>
